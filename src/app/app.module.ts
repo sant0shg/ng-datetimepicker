3a02c32e20c288a624db35b047988cf293cb753e
SANTOSH <santosh@mac.local>
2017-11-14 18:28:23 +0530

    Added styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DateTimePickerModule } from '../../node_modules/ng4-datetimepicker';
+// import { DateTimePickerModule } from '../../node_modules/ng4-datetimepicker';
+import { DateTimePickerModule } from './datetimepicker';
 
 
 @NgModule({
